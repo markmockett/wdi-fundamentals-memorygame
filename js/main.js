e26c49ca952cd4b2ab103258e6d63796bba1aaa0
Mark Mockett <mark_m_editor@MarkMockettsPro.lan>
2018-01-24 09:17:37 -0800

    Updated game logic with Card and Face

--- js/main.js
+++ js/main.js
@@ -9,40 +9,63 @@ var cardFour = "king";
 //console.log("User flipped " + cardThree);
 
 var cards;
-var cardOne;
-var cardTwo;
-var cardThree;
-var cardFour;
-var cardsInPlay;
-var flipCard;
-var cardId;
-var checkForMatch;
+var cardOne; var cardTwo; var cardThree; var cardFour;
+var cardsInPlay; var flipCard; var cardId; var checkForMatch;
 
-cards = ["queen", "queen", "king", "king"];
+cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 cardsInPlay = [];
 
 checkForMatch = function () {
       if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
-      } else {
+      }
+        else {
         console.log("Sorry, try again.");
-    }
+      }
 };
 
 flipCard = function (cardId) {
 
   // Display the card the user just flipped in the console.
-  console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank );
 
   // Add card to array of cards that are in play
-  cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank );
+
+  // Display the image path in the console
+    console.log(cards[cardId].cardImage)
+
+  // Display the suit in the console
+    console.log(cards[cardId].suit)
 
   // Check to see if two cards have been played
   if (cardsInPlay.length === 2) {
     // If so, call the checkForMatch function
-    checkForMatch();
+    checkForMatch(); //Otherwise reset
+    cardsInPlay = []
   }
 };
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
