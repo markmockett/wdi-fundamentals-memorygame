a665ec02531b1445d7ff733bed29efb8d4a76206
Mark Mockett <mark_m_editor@MarkMockettsPro.lan>
2018-01-23 22:16:06 -0800

    created more game logic

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+
+var cards;
+var cardOne;
+var cardTwo;
+var cardThree;
+var cardFour;
+var cardsInPlay;
+
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    }
+else alert("Sorry try again!");
+
+};
